692b9284610d215aeef435d2786a00fe9976a866
LinathanZel <linathan@DESKTOP-RVGNQ5E.localdomain>
2026-07-14 15:20:39 -0400

    Struggle rounding

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2193,6 +2193,8 @@ void SetMoveEffect(struct BattleCalcValues *cv, struct SetEffect *se)
     case MOVE_EFFECT_RECOIL_HP_25: // Struggle
     {
         s32 recoil = (gBattleMons[effectBattler].maxHP) / 4;
+        if (B_UPDATED_MOVE_DATA >= GEN_5 && (gBattleMons[effectBattler].maxHP % 4) >= 2) // Account for standard rounding (Gen5+)
+            recoil++;
         if (recoil == 0)
             recoil = 1;
         SetPassiveDamageAmount(effectBattler, recoil);
